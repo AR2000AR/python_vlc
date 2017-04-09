54255c1cd1f9169fda4dc6fbcced6cf7d48cefe3
AR2000AR <AR2000AR@users.noreply.github.com>
2017-04-09 20:46:50 +0200

    Add files via upload
    Use to add web flux in the correct file

--- Data_base_edit.py
+++ Data_base_edit.py
@@ -3,20 +3,20 @@ import soundcloud
 import os
 from shutil import *
 
-client_id = "F4GaA9l45LjxV77qgsQ75tzRmC0MJFHu"
+client_id = "" #you need your own id
 client = soundcloud.Client(client_id=client_id)
 
 print("DATA BASE EDITOR FOR VLC_interface")
 print("==================================")
 add = input("add : ")
 if add[0:23] == "https://soundcloud.com/":
-    file = open("/home/pi/Remi/Chaine/SoundCloud.txt","a")
+    file = open("SoundCloud.txt","a")
     f = client.get("/resolve",url=add)
     file.write(add+";"+f.title+"\n")
     print(f.title+" : added")
     file.close()
 else:
     name = input("name : ")
-    file = open("/home/pi/Remi/Chaine/Radio.txt","a")
+    file = open("Radio.txt","a")
     file.write(add+";"+name+"\n")
     file.close()
